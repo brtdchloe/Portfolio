47be9017158f651bea4d00b332b91e7ef38d0ee9
brtdchloe <chloe.bertaud28@gmail.com>
2025-03-09 18:05:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-// We get these elements with their id
 const notesContainer = document.getElementById("notes-container")
 const addNote = document.getElementById("add-note")
 const addList = document.getElementById("add-list")
 
-// This code will executes when addNote button is cliked
 addNote.addEventListener("click", () => {
-// We create variables that will be displayed in the HTML file
     let note = document.createElement("div")
     note.className = "note"
     let title = document.createElement("h1")
@@ -17,34 +14,38 @@ addNote.addEventListener("click", () => {
     p.contentEditable = true
     const deleteBtn = document.createElement("button")
     deleteBtn.className = "delete"
+    deleteBtn.id = "delete-note"
     deleteBtn.textContent = "\u00d7"
-    // The whole note is removed/deleted
     deleteBtn.addEventListener("click", () => {
         note.remove()
     });
-// We add the elements we've created before
     note.appendChild(title)
     note.appendChild(p)
     note.appendChild(deleteBtn)
     notesContainer.appendChild(note)
 });
 
-// This code will executes when addList button is clicked
 addList.addEventListener("click", () => {
-// Creation of some elements
     let list = document.createElement("div");
     list.className = "list";
     let title = document.createElement("h1");
     title.className = "title";
     title.textContent = "Title";
     title.contentEditable = true;
+    const deleteBtn = document.createElement("button")
+    deleteBtn.className = "delete"
+    deleteBtn.id = "delete-list"
+    deleteBtn.textContent = "\u00d7"
+    deleteBtn.addEventListener("click", () => {
+        list.remove()
+    });
+    list.appendChild(deleteBtn);
     let span = document.createElement("span");
     span.id = "add-li";
     span.textContent = "+ list item";
     list.appendChild(title);
     list.appendChild(span);
     let ul = document.createElement("ul");
-// Creation of an element in the note
     span.addEventListener("click", () => {
         let li = document.createElement("li");
         let liContent = document.createElement("input");
@@ -56,27 +57,22 @@ addList.addEventListener("click", () => {
         check.addEventListener("click", () => {
             liContent.classList.toggle('checked'); 
             check.classList.toggle('checked');
-        });      
-        let deleteBtn = document.createElement("button");
-        deleteBtn.textContent = "\u00d7";
-        deleteBtn.className = "delete";
-// When this deleteBtn is clicked one of the note's elements is deleted
-        deleteBtn.addEventListener("click", () => {
+        });          
+        const deleteItem = document.createElement("button");
+        deleteItem.textContent = "\u00d7";
+        deleteItem.className = "delete";
+        deleteItem.addEventListener("click", () => {
             li.remove()
         });
-// We add all the elements to the li element, then to the ul element
         li.appendChild(check);
         li.appendChild(liContent);
-        li.appendChild(deleteBtn);
+        li.appendChild(deleteItem);
         ul.appendChild(li);
     });
-// We add the ul element to the list
     list.appendChild(ul);
-// We add the list to our notesContainer
     notesContainer.appendChild(list);
 });
 
-// I tried to create some functions but it actually didn't work...
 // function sauvegarde(){
 //     localStorage.setItem('data', notesContainer.innerHTML);
 // }
