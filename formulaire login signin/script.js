ae05406b829f611e56559bfc7705db6c920ca8c4
Chloé BERTAUD <chloe.bertaud28@gmail.com>
2025-03-16 12:13:02 +0100

    Add files via upload
    This is a login/signin simple form. I used HTML/CSS for it. This is a front-end project.

---
+++ formulaire login signin/script.js
@@ -0,0 +1,13 @@
+function signin() {
+  const signin = document.getElementById("signin");
+  signin.hidden = !signin.hidden;
+  const login = document.getElementById("login");
+  login.hidden = !login.hidden;
+}
+
+function login() {
+  const login = document.getElementById("login");
+  login.hidden = !login.hidden;
+  const signin = document.getElementById("signin");
+  signin.hidden = !signin.hidden;
+}
